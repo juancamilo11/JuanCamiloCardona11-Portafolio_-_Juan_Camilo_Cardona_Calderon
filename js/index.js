80646e2f07409679e062d1850b36bf1127eb8dc0
Juan Camilo Cardona Calderón <juan.cardona69@udea.edu.co>
2021-06-03 19:29:30 -0500

    Loader corregido

---
+++ js/index.js
@@ -0,0 +1,86 @@
+//IIFE (Inmediatly Invoked Function Expretions) for the color theme switch
+((d) => {
+   const $btnSwitch = d.querySelector('#switch');
+   
+   $btnSwitch.addEventListener('click',() => {
+      d.body.classList.toggle('dark');
+      $btnSwitch.classList.toggle('active');
+   });
+})(document);
+
+
+//IIFE (Inmediatly Invoked Function Expretions) for the page's loader
+((d,w) => {
+
+   //Simple loader (Without 'Under construction')
+
+   // window.addEventListener('load',() => {
+   //    let $loader = d.querySelector('.loader');
+   //    $loader.classList.toggle('loader2');
+   // });
+
+
+//Loader with construction website template
+
+   w.addEventListener('load',() => {
+      const $loader = d.querySelector('.loader'),
+         $image = d.querySelector('.loader__img'),
+         $enterCount = d.querySelector('.enter-count');
+      let timeLeft = 5;   //-->5
+
+      $image.setAttribute('src','img/website-construction.jpg');
+      setTimeout(() => {
+         $image.classList.replace('loader__img','loader2__img');
+      }, 50);
+      setInterval(() => {
+         $enterCount.textContent = `${timeLeft--} seconds to go!`;
+      }, 1000);
+      setTimeout(() => {
+         $loader.classList.toggle('loader2');
+      }, 6500);
+   });
+})(document, window);
+
+//IIFE (Inmediatly Invoked Function Expretions) for the authomatic slider
+((d)=> {
+ 
+   const $slider = d.querySelector("#slider");
+   let $sliderSection = d.querySelectorAll(".slider__section"),
+      $sliderSectionLast = $sliderSection[$sliderSection.length - 1];
+
+   const $btnLeft = d.querySelector("#btn-left"),
+      $btnRight = d.querySelector("#btn-right");
+
+   $slider.insertAdjacentElement("afterbegin", $sliderSectionLast);
+
+   function nextCertificate() {
+      let $sliderSectionFirst = d.querySelectorAll(".slider__section")[0];
+      $slider.style.marginLeft = "-200%";
+      $slider.style.transition = "all 0.5s";
+      setTimeout(function(){
+         $slider.style.transition = "none";
+         $slider.insertAdjacentElement("beforeend",$sliderSectionFirst);
+         $slider.style.marginLeft = "-100%";
+      },500);
+   }
+
+   function previusCertificate() {
+      let $sliderSection = d.querySelectorAll(".slider__section");
+      $sliderSectionLast = $sliderSection[$sliderSection.length - 1];
+      $slider.style.marginLeft = "-200%";
+      $slider.style.transition = "all 0.5s"; 
+      setTimeout(() => {
+         $slider.style.transition = "none";
+         $slider.insertAdjacentElement("afterbegin",$sliderSectionLast);
+         $slider.style.marginLeft = "-100%";
+      },500);
+   }
+
+   $btnRight.addEventListener("click" , nextCertificate);
+   $btnLeft.addEventListener("click" , previusCertificate);
+
+   //Next certificate each six seconds -> The slider goes forward
+   setInterval(nextCertificate,5000);
+
+})(document);
+
